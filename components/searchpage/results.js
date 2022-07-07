0b3872ce2d971275fd84661804c326c27a19c1fa
arielahr <arielah1997@gmail.com>
2022-07-07 20:51:26 +0300

    Fixed fetch from db & pagination bug

--- components/searchpage/results.js
+++ components/searchpage/results.js
@@ -1,65 +1,46 @@
-import {
-    Container,
-    Box,
-    Text,
-    Stack,
-    Heading,
-    Link,
-    HStack,
-    IconButton,
-    Button
-} from '@chakra-ui/react'
-import { useState } from 'react'
-import NoResultsFound from '../noresults'
-import {
-    ChevronLeftIcon,
-    ChevronRightIcon
-} from '@chakra-ui/icons'
-import NextLink from 'next/link'
+import { Container, Box, Text, Stack, Heading, Link, HStack, IconButton, Button } from '@chakra-ui/react';
+import { useState } from 'react';
+import NoResultsFound from '../noresults';
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
+import NextLink from 'next/link';
 
 const Pagination = ({ count, currentPage, q, nextPage, prevPage }) => {
     // TODO: Fixated max pages to display
     // Meaning if there are more than 6 pages, display 6
     // When 6 is reachd display relativly more - max 6 button at a time
-    const pages = count >= 60 ? [...Array(Math.ceil((count / 10))).keys()] : [...Array(Math.ceil((count / 10))).keys()]
+    const pages = count >= 60 ? [...Array(Math.ceil(count / 10)).keys()] : [...Array(Math.ceil(count / 10)).keys()];
 
     return (
-        <HStack
-            mt={8}
-            justify="center">
-            <IconButton
-                onClick={prevPage}
-                icon={<ChevronLeftIcon />} />
+        <HStack mt={8} justify='center'>
+            <IconButton onClick={prevPage} icon={<ChevronLeftIcon />} />
             {pages.map((page, index) => {
+                console.log(page + 1 == currentPage);
                 return (
                     <Box key={index}>
-                        <NextLink href={`/search?q=${q}&page=${page + 1}`}>
-                            <Link>
-                                <Button bg={page + 1 == currentPage && 'gray.200'}>{page + 1}</Button>
-                            </Link>
-                        </NextLink>
+                        <Link href={`/search?q=${q}&page=${page + 1}`}>
+                            <Button bg={page + 1 === parseInt(currentPage) && 'gray.200'}>{page + 1}</Button>
+                        </Link>
                     </Box>
-                )
+                );
             })}
-            <IconButton
-                onClick={nextPage}
-                icon={<ChevronRightIcon />} />
+            <IconButton onClick={nextPage} icon={<ChevronRightIcon />} />
         </HStack>
-    )
-}
+    );
+};
 
 const ViewResults = ({ data, page }) => {
-    const [currentPage, setCurrentPage] = useState(isNaN(page) ? 1 : page)
-    const nextPage = () => setCurrentPage(currentPage + 1)
-    const prevPage = () => currentPage !== 0 && setCurrentPage(currentPage - 1)
+    const [currentPage, setCurrentPage] = useState(isNaN(page) ? 1 : page);
+    const nextPage = () => setCurrentPage(currentPage + 1);
+    const prevPage = () => currentPage !== 0 && setCurrentPage(currentPage - 1);
 
-    if (data.count === 0 || data.searchTerm === '') return <NoResultsFound />
+    if (data.count === 0 || data.searchTerm === '') return <NoResultsFound />;
     else {
         return (
             <Box>
-                <Text>Showing {data.count} results for {data.searchTerm}</Text>
-                <Box
-                    p={5}>
+                <Text>
+                    Showing {data.count} results for {data.searchTerm}
+                </Text>
+                <Box p={5}>
                     <Stack spacing={9}>
                         {data.results.map((item, index) => {
                             return (
@@ -67,14 +48,18 @@ const ViewResults = ({ data, page }) => {
                                     <Box>
                                         <NextLink href={`/post/${item._id}`}>
                                             <Link>
-                                                <Heading mb={0} size="md" color={'blue.600'}>{item.title}</Heading>
+                                                <Heading mb={0} size='md' color={'blue.600'}>
+                                                    {item.title}
+                                                </Heading>
                                             </Link>
                                         </NextLink>
                                         <Text fontSize={'.9rem'}>Author: {item.authorName}</Text>
                                     </Box>
-                                    <Text mt={1} as="p">{item.description}</Text>
+                                    <Text mt={1} as='p'>
+                                        {item.description}
+                                    </Text>
                                 </Container>
-                            )
+                            );
                         })}
                     </Stack>
                 </Box>
@@ -83,10 +68,11 @@ const ViewResults = ({ data, page }) => {
                     count={data.count}
                     currentPage={currentPage}
                     nextPage={nextPage}
-                    prevPage={prevPage} />
-            </Box >
-        )
+                    prevPage={prevPage}
+                />
+            </Box>
+        );
     }
-}
+};
 
-export default ViewResults
+export default ViewResults;
--- pages/post/[id].js
+++ pages/post/[id].js
@@ -11,86 +11,90 @@ import ShowActionsBar from '../../components/postowneractions';
 import CommentsSection from '../../components/comments';
 
 export async function getServerSideProps(context) {
-  const { id } = context.query;
+    const { id } = context.query;
 
-  const data = await axios.get(`http://localhost:3000/api/actions/get?id=${id}`).catch(e => console.log(e));
-  const post = data ? data.data.post : null;
+    const url = process.env.PROD ? process.env.PROD : process.env.LOCAL;
 
-  return {
-    props: { post },
-  };
+    const data = await axios.get(`${url}/api/actions/get?id=${id}`).catch(e => console.log(e));
+    const post = data ? data.data.post : null;
+
+    return {
+        props: { post }
+    };
 }
 
 const Post = ({ post }) => {
-  const { data: session, status } = useSession();
+    const { data: session, status } = useSession();
 
-  if (status === 'loading') return <LoadingComponent />;
-  if (!post) return <FourOFour />;
-  if (post.isLocked && !session) return <PostLocked />;
+    if (status === 'loading') return <LoadingComponent />;
+    if (!post) return <FourOFour />;
+    if (post.isLocked && !session) return <PostLocked />;
 
-  const postDate = new Date(post.createdAt);
-  const dateSeparator = '/';
-  const createdAt = `${postDate.getUTCDate() > 9 ? postDate.getUTCDate() : '0' + postDate.getUTCDate()}${dateSeparator}${
-    postDate.getUTCMonth() + 1 > 9 ? postDate.getUTCMonth() + 1 : '0' + (postDate.getUTCMonth() + 1)
-  }${dateSeparator}${postDate.getUTCFullYear()}`;
+    const postDate = new Date(post.createdAt);
+    const dateSeparator = '/';
+    const createdAt = `${postDate.getUTCDate() > 9 ? postDate.getUTCDate() : '0' + postDate.getUTCDate()}${dateSeparator}${
+        postDate.getUTCMonth() + 1 > 9 ? postDate.getUTCMonth() + 1 : '0' + (postDate.getUTCMonth() + 1)
+    }${dateSeparator}${postDate.getUTCFullYear()}`;
 
-  const postUpdate = new Date(post.lastUpdated);
+    const postUpdate = new Date(post.lastUpdated);
 
-  const lastUpdated = `${postUpdate.getUTCDate() > 9 ? postUpdate.getUTCDate() : '0' + postUpdate.getUTCDate()}${dateSeparator}${
-    postUpdate.getUTCMonth() + 1 > 9 ? postUpdate.getUTCMonth() + 1 : '0' + (postUpdate.getUTCMonth() + 1)
-  }${dateSeparator}${postUpdate.getUTCFullYear()}`;
+    const lastUpdated = `${
+        postUpdate.getUTCDate() > 9 ? postUpdate.getUTCDate() : '0' + postUpdate.getUTCDate()
+    }${dateSeparator}${
+        postUpdate.getUTCMonth() + 1 > 9 ? postUpdate.getUTCMonth() + 1 : '0' + (postUpdate.getUTCMonth() + 1)
+    }${dateSeparator}${postUpdate.getUTCFullYear()}`;
 
-  return (
-    <AnimationLayout>
-      <Head>
-        <title>
-          {post.topicID} || {post.title}
-        </title>
-      </Head>
-      <Layout>
-        {post.authorID === session?.user?.email && (
-          <Box align="right" my={5}>
-            <ShowActionsBar post={post} />
-          </Box>
-        )}
-        <Box align="center" mb={8}>
-          <Box bgColor="gray.200" px={{ base: 5, md: 8 }} pt={5} pb={8} maxWidth={'max-content'}>
-            <Heading size="3xl" fontStyle={'italic'}>
-              {post.topicID}
-            </Heading>
-          </Box>
-        </Box>
-        <Box>
-          <Heading as="h1" size="xl">
-            {post.title}
-          </Heading>
-          <Text fontSize={'.8em'}>
-            Posted by: {post.authorName} {`\/\/`} {createdAt}
-          </Text>
-          {post.lastUpdated && (
-            <Text fontStyle={'italic'} color="gray.600" fontSize=".7em">
-              Last edited at {lastUpdated}
-            </Text>
-          )}
-          <Divider my={2} />
-        </Box>
-        <Box p={2}>
-          <Heading as="h4" size="sm">
-            Description:
-          </Heading>
-          <Text ml={4}>{post.description}</Text>
-        </Box>
-        <Divider my={2} />
-        <Box align="center" mt={3}>
-          <Link href={post.fileUrl} style={{ textDecoration: 'none' }}>
-            <Button colorScheme={'linkedin'}>Download Summary</Button>
-          </Link>
-        </Box>
-        <Divider my={2} />
-        <CommentsSection comments={post.comments} />
-      </Layout>
-    </AnimationLayout>
-  );
+    return (
+        <AnimationLayout>
+            <Head>
+                <title>
+                    {post.topicID} || {post.title}
+                </title>
+            </Head>
+            <Layout>
+                {post.authorID === session?.user?.email && (
+                    <Box align='right' my={5}>
+                        <ShowActionsBar post={post} />
+                    </Box>
+                )}
+                <Box align='center' mb={8}>
+                    <Box bgColor='gray.200' px={{ base: 5, md: 8 }} pt={5} pb={8} maxWidth={'max-content'}>
+                        <Heading size='3xl' fontStyle={'italic'}>
+                            {post.topicID}
+                        </Heading>
+                    </Box>
+                </Box>
+                <Box>
+                    <Heading as='h1' size='xl'>
+                        {post.title}
+                    </Heading>
+                    <Text fontSize={'.8em'}>
+                        Posted by: {post.authorName} {`\/\/`} {createdAt}
+                    </Text>
+                    {post.lastUpdated && (
+                        <Text fontStyle={'italic'} color='gray.600' fontSize='.7em'>
+                            Last edited at {lastUpdated}
+                        </Text>
+                    )}
+                    <Divider my={2} />
+                </Box>
+                <Box p={2}>
+                    <Heading as='h4' size='sm'>
+                        Description:
+                    </Heading>
+                    <Text ml={4}>{post.description}</Text>
+                </Box>
+                <Divider my={2} />
+                <Box align='center' mt={3}>
+                    <Link href={post.fileUrl} style={{ textDecoration: 'none' }}>
+                        <Button colorScheme={'linkedin'}>Download Summary</Button>
+                    </Link>
+                </Box>
+                <Divider my={2} />
+                <CommentsSection comments={post.comments} />
+            </Layout>
+        </AnimationLayout>
+    );
 };
 
 export default Post;
