58eaf6846471facadd40e9911e9411f6b2e9121a
arielahr <arielah1997@gmail.com>
2022-07-07 19:29:18 +0300

    test

--- components/searchbar.js
+++ components/searchbar.js
@@ -1,41 +1,37 @@
-import {
-    Input,
-    InputRightAddon, InputRightElement,
-    InputGroup,
-    Button
-} from '@chakra-ui/react'
-import { SearchIcon } from '@chakra-ui/icons'
-import { useState } from 'react'
-import { useRouter } from 'next/router'
+import { Input, InputRightAddon, InputRightElement, InputGroup, Button } from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
+import { useState } from 'react/cjs/react.development';
+import { useRouter } from 'next/router';
 
 const SearchInput = ({ value }) => {
-    const [searchValue, setSearchValue] = useState('')
-    const Router = useRouter()
+    const [searchValue, setSearchValue] = useState('');
+    const Router = useRouter();
 
-    const onSubmit = (e) => {
-        if (searchValue !== '') Router.push(`/search?q=${searchValue.trim()}`)
-        e.preventDefault()
-    }
+    const onSubmit = e => {
+        if (searchValue !== '') Router.push(`/search?q=${searchValue.trim()}`);
+        e.preventDefault();
+    };
 
     return (
-        <form onSubmit={(e) => onSubmit(e)}>
+        <form onSubmit={e => onSubmit(e)}>
             <InputGroup mt={10} maxW='90%' size={{ base: 'sm', md: 'md' }} m={0}>
                 <Input
                     onChange={e => setSearchValue(e.target.value)}
-                    type="text"
+                    type='text'
                     name='q'
                     bg={'white'}
                     defaultValue={value && value}
                     size={{ base: 'sm', md: 'md' }}
-                    placeholder="Search for knowlage..." />
+                    placeholder='Search for knowlage...'
+                />
                 <InputRightElement p={0}>
-                    <Button type="submit" isDisabled={searchValue === ''} size={{ base: 'sm', md: 'md' }}>
+                    <Button type='submit' isDisabled={searchValue === ''} size={{ base: 'sm', md: 'md' }}>
                         <SearchIcon />
                     </Button>
                 </InputRightElement>
             </InputGroup>
-        </form >
-    )
-}
+        </form>
+    );
+};
 
-export default SearchInput
+export default SearchInput;
