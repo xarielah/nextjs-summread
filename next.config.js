791108a3d8c324eb6c11b57f4ccb482af2faacd7
arielahr <arielah1997@gmail.com>
2022-07-07 20:02:49 +0300

    NextAuth bug fixed

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
     reactStrictMode: true,
     env: {
         LOCAL: 'http://localhost:3000',
-        PROD: 'https://nextjs-summread.vercel.app'
+        PROD: 'https://nextjs-summread.vercel.app',
+        NEXTAUTH_SECRET: '4CA7E2EAF98ED497E0DBF2014CE870957E4122050659D964F98EF56B58199D75'
     }
 };
 
